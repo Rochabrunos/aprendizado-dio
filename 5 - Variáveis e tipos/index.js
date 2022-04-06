6f883e0daff0939277105075c9a754d770c457bc
Bruno Rocha <bsrocha1005@gmail.com>
2022-04-06 14:35:07 -0300

    conclusao do curso: variaveis e tipos

---
+++ 5 - Variáveis e tipos/index.js
@@ -0,0 +1,10 @@
+var firstName = "Bruno";
+var lastName = "Souza";
+
+if (firstName === "Bruno") {
+	let firstName = "Lucas";
+	var lastName = "Rocha";
+
+	console.log(firstName, lastName);
+}
+console.log(firstName, lastName);
