73e7fb02bf69c58f4b505061bc149aac42279b4c
Bruno Rocha <bsrocha1005@gmail.com>
2022-04-06 16:27:08 -0300

    conclusao curso: modulos em js

---
+++ 6 - Trabalhando com módulos em JavaScript/functions.mjs
@@ -0,0 +1,11 @@
+function showAge(name, age) {
+	return `A idade de ${name} é ${age}`;
+}
+function showCity(name, city) {
+	return `A cidade de ${name} é ${city}`;
+}
+function showHobby(name, hobby) {
+	return `A hobby de ${name} é ${hobby}`;
+}
+
+export { showAge, showCity, showHobby };
---
+++ 6 - Trabalhando com módulos em JavaScript/main.js
@@ -0,0 +1,10 @@
+import { showAge, showCity, showHobby } from "./functions.mjs";
+
+const name = "Bruno";
+const age = 26;
+const city = "Congonhas";
+const hobby = "Música";
+
+console.log(showAge(name, age));
+console.log(showCity(name, city));
+console.log(showHobby(name, hobby));
